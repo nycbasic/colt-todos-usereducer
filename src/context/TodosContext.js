b17409660d9b557f0a1e97ea84818e0a10223d77
nycbasic <samuel.poon@baruchmail.cuny.edu>
2020-12-26 14:07:51 -0500

    adding another custom hook for adding data to local storage

--- src/context/TodosContext.js
+++ src/context/TodosContext.js
@@ -1,5 +1,7 @@
 import React, {createContext, useReducer} from 'react';
 import todoReducer from "../reducer/todoReducer";
+import {useLocalStorageReducer} from "../hooks/useLocalStorageReducer"
+
 
 const init = [
     {id: 1, task: "Mow the lawn using goats", completed: false},
@@ -10,7 +12,7 @@ export const TodosContext = createContext(init);
 export const DispatchContext = createContext();
 
 export const TodosProvider = (props) => {
-    const [todos, dispatch] = useReducer(todoReducer, init)
+    const [todos, dispatch] = useLocalStorageReducer("todos", init, todoReducer)
 
     return (
         <TodosContext.Provider value={todos}>
